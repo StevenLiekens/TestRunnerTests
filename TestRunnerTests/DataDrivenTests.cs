a524224b92cd30a281c00b74f40158ecf7b0a86f
Steven Liekens <steven.liekens@gmail.com>
2015-09-28 19:53:41 +0200

    Initial commit

---
+++ TestRunnerTests/DataDrivenTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TestRunnerTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Xunit;
+
+    public class DataDrivenTests
+    {
+        public static IEnumerable<object[]> GetNumbers()
+        {
+            const int Size = 10000;
+            var numbers = new List<object[]>(Size);
+            numbers.AddRange(Enumerable.Range(1, Size).Select(i => new object[] { i }));
+            return numbers;
+        }
+
+        [Theory]
+        [MemberData("GetNumbers")]
+        public void Test(int x)
+        {
+            Assert.True(x > 0);
+        }
+    }
+}
